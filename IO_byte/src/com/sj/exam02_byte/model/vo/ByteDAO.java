f363de46386aedc91dae03f11b8915537a541e76
LeeSeonJe <tjswp012@gamil.com>
2020-02-03 01:27:02 +0900

    IO_Byte commit

--- IO_byte/src/com/sj/exam02_byte/model/vo/ByteDAO.java
+++ IO_byte/src/com/sj/exam02_byte/model/vo/ByteDAO.java
@@ -7,33 +7,65 @@ import java.io.IOException;
 
 public class ByteDAO {
 	public void fileOpen() {
+		FileInputStream fis = null;
 		try {
-			FileInputStream fis = new FileInputStream("exam02_byte.txt");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+			fis = new FileInputStream("exam02_byte.txt");
+			// byte ë‹¨ìœ„ í•˜ë‚˜ë¥¼ ì½ì–´ì˜´
+			int value = 0;
+			while ((value = fis.read()) != -1) {
+				System.out.print((char) value + " ");
+			}
 
-	public void fileSave() {
-		try {
-			FileOutputStream fos = new FileOutputStream("exam02_byte.txt");
-			// int °ª ÀúÀå
-			fos.write(97);
-			
-			// byte ¹è¿­ »ı¼º
-			byte[] b = new byte[] { 'a', 'b', 'c', 'd', 'e' };
-			// ¹è¿­ ÀüÃ¼ ÀúÀå
-			fos.write(b);
-			
-			// ¹è¿­ ÀÎµ¦½º ¹üÀ§ ÀúÀå
-			fos.write(b, 1, 3);
-			
+//			
+//			byte[] b1 = new byte[10];
+//			fis.read(b1);
+//			fis.read(b1, 0, 4);
+//			
+//			for(int a : b1) {
+//				System.out.println(a);
+//			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				fis.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void fileSave() {
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream("exam02_byte.txt");
+			// int ê°’ ì €ì¥
+			fos.write(97);
+
+			// byte ë°°ì—´ ìƒì„±
+			byte[] b = new byte[] { 'a', 'b', 'c', 'd', 'e' };
+			// ë°°ì—´ ì „ì²´ ì €ì¥
+			fos.write(b);
+
+			// ë°°ì—´ ì¸ë±ìŠ¤ ë²”ìœ„ ì €ì¥
+			fos.write(b, 1, 3);
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fos.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
--- IO_byte/src/com/sj/exam02_byte/run/Run.java
+++ IO_byte/src/com/sj/exam02_byte/run/Run.java
@@ -6,5 +6,6 @@ public class Run {
 	public static void main(String[] args) {
 		ByteDAO bd = new ByteDAO();
 		bd.fileSave();
+		bd.fileOpen();
 	}
 }
